0dd3d8109ce7dcc73c7661c14ebd69d4c5257e4b
dieploegskaar <109988398+dieploegskaar@users.noreply.github.com>
2022-09-08 09:23:14 +0200

    Add files via upload

--- BMP280_Conviron.py
+++ BMP280_Conviron.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import tkinter as tk
 import threading
 import time
 import datetime
@@ -7,6 +6,7 @@ import RPi.GPIO as GPIO
 from bmp280 import BMP280
 import psutil
 import csv
+import os
 
 Mrng_MaxTemp = 8.0                      #Define Morning Tempreature
 Day_MaxTemp = 15.0                      #Define Day Tempreature
@@ -78,24 +78,19 @@ def Lighting_Timer():
 def Cooling():
     GPIO.output(26,False)   #Heating OFF
     GPIO.output(17,True)    #cooling ON
-    print("Cooling ON\n")
-    #time.sleep(14.0)
-    #GPIO.output(17,False)   #Cooling OFF
-    #time.sleep(100.0)
 
 def Heating():
     GPIO.output(17,False)   #Cooling OFF
     GPIO.output(26,True)    #Heating ON
-    print("Heating ON\n")
     time.sleep(12.0)
-    GPIO.output(26,False)   #Heating OFF
-    time.sleep(60.0)
+    GPIO.output(26,False)
+    time.sleep(30.0)
 
 def In_Range():
+    time.sleep(5.0)
     GPIO.output(17,False)   #Cooling OFF
     GPIO.output(26,False)   #Heating OFF
-    print("Cooling & Heating OFF\n")
-    time.sleep(30.0)
+    time.sleep(25.0)
      
 def Temp_Control(): #Tempreature sensing and control
     while True:
@@ -106,15 +101,12 @@ def Temp_Control(): #Tempreature sensing and control
             isDay = now.replace(hour = 11, minute = 0, second  = 0, microsecond = 0)    #Day start
             isEvng =now.replace(hour = 16, minute = 0, second = 0, microsecond = 0)
             isNight = now.replace(hour = 18, minute = 0, second  = 0, microsecond = 0)  #Night start
-
             mrngTemps = now > isMrng and now < isDay
             dayTemps = now > isDay and now < isEvng
             evngTemps = now > isEvng and now < isNight
             nightTemps = now > isNight or now < isMrng 
             #Morning Tempreatire Scenario
             if(mrngTemps == True):
-                print('Morning Tempreatire Scenario')
-                print(temp)
                 if temp > (Mrng_MaxTemp+0.5):
                     Cooling()                    
                 elif temp < (Mrng_MaxTemp-3.0):
@@ -123,8 +115,6 @@ def Temp_Control(): #Tempreature sensing and control
                     In_Range()
             #Day Tempreatue scenario
             if(dayTemps == True):
-                print('Day Tempreatures scenario')
-                print(temp)
                 if temp > (Day_MaxTemp+0.5):
                     Cooling()
                 elif temp < (Day_MaxTemp-3.0):
@@ -133,8 +123,6 @@ def Temp_Control(): #Tempreature sensing and control
                     In_Range() 
             #Evening Tempreature scenario  
             if(evngTemps == True):
-                print('Evening Tempreature scenario')
-                print(temp)
                 if temp > (Evng_Maxtemp+0.5):
                     Cooling()
                 elif temp < (Evng_Maxtemp-3.0):
@@ -143,8 +131,6 @@ def Temp_Control(): #Tempreature sensing and control
                     In_Range()
             #Night Tempreature scenario               
             if(nightTemps == True):
-                print('Night Tempreature scenario')
-                print(temp)
                 if temp > (Night_Maxtemp+0.5):
                     Cooling()
                 elif temp < (Night_Maxtemp-3.0):
@@ -174,43 +160,28 @@ def data_log():
             time.sleep(60)
 
 def out_put():
+    clear = lambda: os.system('clear')
     while True:
-        print("Building E6, room B17 EnviroPi climate data:")
         now = datetime.datetime.now()
         temp = bmp280.get_temperature()         
         pressure = bmp280.get_pressure()
         degree_sign = u"\N{DEGREE SIGN}"
-        format_temp = "{:.2f}".format(temp)
-        print(now.strftime("%c"))
+        format_temp = "{:.3f}".format(temp)
+        print("Building E6, room B17 EnviroPi climate data:")
+        print(now.strftime("%c\n"))
+        if GPIO.input(17):
+            print("Cooling ON!\n")
+        elif GPIO.input(26):
+            print("Heating ON!\n")
+        else:
+            print("Cooling & Heating OFF!\n")
         print('Temperature: ' + format_temp + degree_sign + 'C')
-        format_press = "{:.2f}".format(pressure)
-        print('Pressure: ' + format_press + ' hPa \n')
-        time.sleep(4)
+        format_press = "{:.3f}".format(pressure)
+        print('Pressure: ' + format_press + ' hPa \n') 
+        time.sleep(0.1)
+        clear()
         
-def readsensor():
-    while True:
-        temp = bmp280.get_temperature() 
-        degree_sign = u"\N{DEGREE SIGN}"
-        format_temp = "{:.2f}".format(temp)
-        var.set(format_temp + degree_sign + 'C')
-        
-root = tk.Tk()
-
-lblLocal = tk.Label(root, text = "Building E6, room B17 EnviroPi climate data:")
-lblTemp = tk.Label(root, text = "Current Temptrature:")
-lblLocal.pack()
-lblTemp.pack()
-var = tk.StringVar()
-TempVaLlbl = tk.Label(root, textvariable=var, width=20, height=5, font=('Consolas', 24, 'bold'))
-TempVaLlbl.pack()
-threading.Thread(target=readsensor).start()
-
 threading.Thread(target=Lighting_Timer).start()
 threading.Thread(target=Temp_Control).start()
 threading.Thread(target=data_log).start()
-threading.Thread(target=out_put).start()
-root.mainloop()
-
-
-        
-
+threading.Thread(target=out_put).start() 
